b1f2067f78d0ea466a515409d3ebfa0e79cbe9ad
nutpantz <12160507+nutpantz@users.noreply.github.com>
2026-07-02 15:36:54 -0700

    Add service checks for multiple services in script

--- servicecheck.sh
+++ servicecheck.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+echo sddm    
 systemctl list-unit-files --type=service --state=enabled | grep "enabled" | grep "sddm"
+echo forgejo
 systemctl list-unit-files --type=service --state=enabled | grep "enabled" | grep "forgejo"
+echo radicle
 systemctl list-unit-files --type=service --state=enabled | grep "enabled" | grep "radicale"
+echo sftpgo
 systemctl list-unit-files --type=service --state=enabled | grep "enabled" | grep "sftpgo"
+echo ufw
 systemctl list-unit-files --type=service --state=enabled | grep "enabled" | grep "ufw"
+echo vncserver
 ps -ef | grep x0vncserver
+echo ________________________________________
 free -h
