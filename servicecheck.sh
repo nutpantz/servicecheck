67ce5e2c739f75eba4acc2481e0f62f86a76ef0d
nutpantz <12160507+nutpantz@users.noreply.github.com>
2026-07-02 19:57:46 -0700

    Fix grep commands for service checks in script

--- servicecheck.sh
+++ servicecheck.sh
@@ -2,19 +2,19 @@
 echo sddm    
 systemctl list-unit-files --type=service --state=enabled | grep "active" | grep "sddm"
 echo forgejo
-systemctl list-unit-files --type=service --state=enabled | grep "active"" | grep "forgejo"
+systemctl list-unit-files --type=service --state=enabled | grep "active" | grep "forgejo"
 echo radicle
-systemctl list-unit-files --type=service --state=enabled | grep "active"" | grep "radicale"
+systemctl list-unit-files --type=service --state=enabled | grep "active" | grep "radicale"
 echo sftpgo
-systemctl list-unit-files --type=service --state=enabled | grep "active"" | grep "sftpgo"
+systemctl list-unit-files --type=service --state=enabled | grep "active" | grep "sftpgo"
 echo ufw
-systemctl list-unit-files --type=service --state=enabled | grep "active"" | grep "ufw"
+systemctl list-unit-files --type=service --state=enabled | grep "active" | grep "ufw"
 echo vncserver
 systemctl status tigervncserver@:1 | grep "enabled"
 echo ________________________________________
 free -h
-systemctl status sddm | grep "active""
-systemctl status forgejo | grep "active""
-systemctl status radicale | grep "active""
-systemctl status sftpgo | grep "active""
-systemctl status ufw | grep "active""
+systemctl status sddm | grep "active"
+systemctl status forgejo | grep "active"
+systemctl status radicale | grep "active"
+systemctl status sftpgo | grep "active"
+systemctl status ufw | grep "active"
